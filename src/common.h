6f9aface96a9118e6e0bd484603211904bc11b7b
Hendrik Borghorst <hendrikborghorst@gmail.com>
2017-08-03 21:52:32 +0200

    Version 1.3.3 release

--- src/common.h
+++ src/common.h
@@ -7,7 +7,7 @@
 // ONLY FOR USE WITH JOLLAMPC
 #define LASTFMAPIKEY "1c728df8f626849518971eaae29e01a1"
 #define LASTFMDEFAULTSIZE "mega"
-#define VERSION "1.3.2"
+#define VERSION "1.3.3"
 
 #define IMAGEPROVIDERNAME "imagedbprovider"
 #define DUMMY_ALBUMIMAGE "image://theme/icon-l-music"
